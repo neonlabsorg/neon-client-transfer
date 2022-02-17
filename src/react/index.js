73eaf5d7ac4db5d611fac98764f32b20ea0d0415
Bejoy <casperyourweb@gmail.com>
2022-02-17 22:06:18 +0300

    fix hook

--- src/react/index.js
+++ src/react/index.js
@@ -2,8 +2,8 @@ import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { useWeb3React } from '@web3-react/core'
 import NeonPortal from '../NeonPortal'
 
-export const useNeonTransfer = (events) => {
-  const connection = useConnection()
+const useNeonTransfer = (events) => {
+  const { connection }  = useConnection()
   const { account } = useWeb3React()
   const { publicKey } = useWallet()
   let portal = new NeonPortal({
@@ -15,3 +15,4 @@ export const useNeonTransfer = (events) => {
   const createSolanaTransfer = portal.createSolanaTransfer.bind(portal, events)
   return { createNeonTransfer, createSolanaTransfer }
 }
+export default useNeonTransfer
