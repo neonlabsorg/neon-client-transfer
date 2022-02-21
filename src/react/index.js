bb98fb80924e2d85097d2452737fe71ea8f6b73f
Bejoy <casperyourweb@gmail.com>
2022-02-21 12:13:58 +0300

    fix core

--- src/react/index.js
+++ src/react/index.js
@@ -3,7 +3,7 @@ import { useWeb3React } from '@web3-react/core'
 import NeonPortal from '../NeonPortal'
 
 const useNeonTransfer = (events) => {
-  const { connection }  = useSolanaConnection()
+  const { connection }  = useConnection()
   const { account } = useWeb3React()
   const { publicKey } = useWallet()
   let portal = new NeonPortal({
