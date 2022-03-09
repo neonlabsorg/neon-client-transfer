14e9e4983ef3addd22be22e3c7260aad3700ffc4
webkieth <firekit13@gmail.com>
2022-03-09 12:41:09 +0300

    add current connection arg for react hook

--- src/react/index.js
+++ src/react/index.js
@@ -2,14 +2,14 @@ import { useWallet, useConnection } from '@solana/wallet-adapter-react'
 import { useWeb3React } from '@web3-react/core'
 import NeonPortal from '../NeonPortal'
 
-const useNeonTransfer = (events) => {
+const useNeonTransfer = (events, currentConnection) => {
   const { connection }  = useConnection()
   const { account } = useWeb3React()
   const { publicKey } = useWallet()
   let portal = new NeonPortal({
     solanaWalletAddress: publicKey,
     neonWalletAddress: account,
-    customConnection: connection
+    customConnection: currentConnection ? currentConnection : connection
   })
   const createNeonTransfer = portal.createNeonTransfer.bind(portal, events)
   const createSolanaTransfer = portal.createSolanaTransfer.bind(portal, events)
